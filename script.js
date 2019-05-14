5c29b6188b70cd8a4ca277fb7d96320c284c260d
nleavi11 <noahjlvtt@gmail.com>
2019-05-13 21:27:03 -0600

    finish changes; add main screen and format layout; need to improve submit button and add to address bar

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.onload = function(){
     return;
     console.log(value);
 
-    const url = "http://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=9d83bb7154110a5055f94e83aad741df";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=9d83bb7154110a5055f94e83aad741df";
 
     try{
       const response = await fetch(url);
@@ -15,7 +15,7 @@ window.onload = function(){
       var results = "";
       results += '<h1>Weather in ' + json.name + "</h1>";
       for (var i=0; i < json.weather.length; i++) {
-        results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
+        results += '<img class="black_and_white"src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
       }
       results += '<h2>' + json.main.temp + " &deg;F</h2>"
       results += "<p>"
@@ -30,7 +30,7 @@ window.onload = function(){
       console.log(err);
     }
 
-    const url2 = "http://api.openweathermap.org/data/2.5/forecast?q=" + value + ", US&units=imperial" + "&APPID=9d83bb7154110a5055f94e83aad741df";
+    const url2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + value + ", US&units=imperial" + "&APPID=9d83bb7154110a5055f94e83aad741df";
     try{
       const response2 = await fetch(url2);
       const json2 = await response2.json();
@@ -38,15 +38,14 @@ window.onload = function(){
       var forecast = "";
       for (var i=0; i < json2.list.length; i++) {
         forecast += '<div class="forecast">'
-        forecast += '<div class="date"><h2>' + moment(json2.list[i].dt_txt).format('MMMM Do YYYY, h a') + '</h2></div>';
-        // 'MMMM Do YYYY, h:mm:ss a'
-        forecast += '<div class="hourly_temp"><p>Temperature: ' + json2.list[i].main.temp + ' &deg;F' + "</p></div>";
-        forecast += '<div class="hourly_picture"><img src="http://openweathermap.org/img/w/' + json2.list[i].weather[0].icon + '.png"/></div>'
+        forecast += '<div class="date"><p>' + moment(json2.list[i].dt_txt).format('MMMM Do h a') + '</p></div>';
+        forecast += '<div class="hourly_temp"><p>' + json2.list[i].main.temp + ' &deg;F' + "</p></div>";
+        forecast += '<div class="hourly_picture"><img class="black_and_white"src="http://openweathermap.org/img/w/' + json2.list[i].weather[0].icon + '.png"/></div>'
         forecast +='</div>'
       }
       document.getElementById("forecastResults").innerHTML = forecast;
-    }catch{
-
+    }catch(err){
+      console.log(err);
     }
   });
 }
